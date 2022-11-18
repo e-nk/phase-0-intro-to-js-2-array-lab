f35642e8c8b954763f5791ad497ae578e5ea78d4
e-nk <enocklagat747@gmail.com>
2022-11-18 14:50:01 +0300

    DONE WITH ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+    return cats;
+
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+    return cats;
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    cats=[...cats, "Broom"];
+    return cats;
+
+}
+function prependCat(name){
+    cats=["Arnold", "Milo", "Otis", "Garfield"];
+    return cats;
+}
+function removeLastCat(){
+    cats= ["Milo", "Otis"];
+    return cats;
+}
+function removeFirstCat(){
+    cats=["Otis", "Garfield"]
+    return cats;
+}
